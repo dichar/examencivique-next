0495978bbdd2c41f87d61a52f702478b38b9c72e
Rachid TRAHIM <trahim@zeturf.com>
2026-03-13 17:25:41 +0100

    fix sitemap

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "examencivique.info" }],
+        destination: "https://www.examencivique.info/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
